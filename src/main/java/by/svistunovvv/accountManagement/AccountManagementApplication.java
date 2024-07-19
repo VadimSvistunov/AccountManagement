5995a21bc32dfe99835e30a7473e55dedd515eb3
VadimSvistunov <vadim.svistunov@onilab.com>
2024-07-19 17:41:36 +0300

    init

---
+++ src/main/java/by/svistunovvv/accountManagement/AccountManagementApplication.java
@@ -0,0 +1,13 @@
+package by.svistunovvv.accountManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+
+}
